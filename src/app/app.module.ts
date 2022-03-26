27e2d9f900f4ceacf23ddaeaac999daa89de3de8
IgnacioGarciaReyna <ignaciogarciareyna@live.com>
2022-03-26 18:55:49 -0300

    Se creó el componente technologies

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { TechnologiesComponent } from './components/technologies/technologies.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    TechnologiesComponent
   ],
   imports: [
     BrowserModule,
