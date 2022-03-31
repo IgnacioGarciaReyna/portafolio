036dff33091f452e5c6cdc275493d0f6c954d711
IgnacioGarciaReyna <ignaciogarciareyna@live.com>
2022-03-31 00:28:57 -0300

    Se creó el método para descargar el Currículum/Resume

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -3,13 +3,21 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  downloadResume() {
+    const downloadInstance = document.createElement('a');
+    downloadInstance.href =
+      './../../../assets/Currículum Vitae - Ignacio Garcia Reyna.pdf';
+    downloadInstance.target = '_blank';
+    downloadInstance.download = 'Resume';
+    document.getElementById('container')?.appendChild(downloadInstance);
+    downloadInstance.click();
+    document.getElementById('container')?.removeChild(downloadInstance);
   }
-
 }
